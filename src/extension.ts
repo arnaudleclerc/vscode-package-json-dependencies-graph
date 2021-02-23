3cc532ced9b9a4cbc83ae8936e9ed7e745c60ce8
Arnaud Leclerc <A.Leclerc@tekaris.com>
2021-02-23 14:18:05 +0100

    Basing decisions on package.json and package-lock.json

--- src/extension.ts
+++ src/extension.ts
@@ -13,47 +13,51 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-package-json-dependencies-graph.parse', () => {
+	let disposable = vscode.commands.registerCommand('vscode-npm-packages-graph.parse', () => {
 		Logger.initialize();
 
 		// The code you place here will be executed every time your command is executed
 		if (vscode.workspace.workspaceFolders !== undefined) {
 			let f = vscode.workspace.workspaceFolders[0].uri.fsPath;
 
-			readFile(`${f}\\package-lock.json`, (err, data) => {
+			readFile(`${f}\\package.json`, (err, data) => {
 				if (err) {
 					vscode.window.showErrorMessage(err.message);
 				} else {
+					readFile(`${f}\\package-lock.json`, (errLock, dataLock) => {
+						if (errLock) {
+							vscode.window.showErrorMessage(errLock.message);
+						} else {
+							const panel = vscode.window.createWebviewPanel(
+								'dependencyGraph', // Identifies the type of the webview. Used internally
+								'Dependency Graph', // Title of the panel displayed to the user
+								vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+								{
+									enableScripts: true
+								} // Webview options. More on these later.
+							);
 
-					const panel = vscode.window.createWebviewPanel(
-						'dependencyGraph', // Identifies the type of the webview. Used internally
-						'Dependency Graph', // Title of the panel displayed to the user
-						vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-						{
-							enableScripts: true
-						} // Webview options. More on these later.
-					);
+							const result = DependenciesParser.parse(JSON.parse(data.toString()), JSON.parse(dataLock.toString()));
+							const nodes = JSON.stringify(result.nodes);
+							const links = JSON.stringify(result.links);
 
-					const result = DependenciesParser.parse(JSON.parse(data.toString()));
+							Logger.info(nodes);
+							Logger.info(links);
 
-					const nodes = JSON.stringify(result.nodes);
-					const links = JSON.stringify(result.links);
+							const d3jsPath = vscode.Uri.file(
+								path.join(context.extensionPath, 'node_modules', 'd3', 'dist/d3.min.js')
+							);
+							const d3ForcePath = vscode.Uri.file(
+								path.join(context.extensionPath, 'node_modules', 'd3-force', 'dist/d3-force.min.js')
+							);
 
-					Logger.info(nodes);
-					Logger.info(links);
+							const d3Src = panel.webview.asWebviewUri(d3jsPath);
+							const d3ForceSrc = panel.webview.asWebviewUri(d3ForcePath);
 
-					const d3jsPath = vscode.Uri.file(
-						path.join(context.extensionPath, 'node_modules', 'd3', 'dist/d3.min.js')
-					);
-					const d3ForcePath = vscode.Uri.file(
-						path.join(context.extensionPath, 'node_modules', 'd3-force', 'dist/d3-force.min.js')
-					);
-
-					const d3Src = panel.webview.asWebviewUri(d3jsPath);
-					const d3ForceSrc = panel.webview.asWebviewUri(d3ForcePath);
-
-					// And set its HTML content
-					panel.webview.html = getWebviewContent(nodes, links, d3Src, d3ForceSrc);
+							// And set its HTML content
+							panel.webview.html = getWebviewContent(nodes, links, d3Src, d3ForceSrc);
+						}
+					});
 				}
 			});
 		}
@@ -77,62 +81,96 @@ function getWebviewContent(nodes: string, links: string, d3Src: vscode.Uri, d3Fo
   <head>
 	  <meta charset="UTF-8">
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
+	  <title>NPM Packages Graph</title>
 	  <script src="${d3Src}"></script>
 	  <script src="${d3ForceSrc}"></script>
   </head>
-  <body style="background-color:'#fff'">
+  <body>
+	<select id="packages"></select>
 	<div id="dependencyGraphContainer">
 	<script>
+
 	const nodes = ${nodes};
 	const links = ${links};
-	const width = 1200;
-	const height = 1200;
 
-	const simulation = d3.forceSimulation(nodes)
-		.force("link", d3.forceLink(links).id(d => d.id))
-		.force("charge", d3.forceManyBody())
-		.force("center", d3.forceCenter(width / 2, height / 2));
+	function generateGraph(selectedPackage) {
 
-	const svg = d3.select('#dependencyGraphContainer')
-		.append('svg')
-		.attr("width", width)
-		.attr('height', height);
+		const svgContainer = document.getElementById('dependencyGraphContainerSvg');
+		if(svgContainer) {
+			svgContainer.remove();
+		}
 
-	const link = svg.append("g")
-		.attr("stroke", "#999")
-		.attr("stroke-opacity", 0.6)
-		.selectAll("line")
-		.data(links)
-		.join("line")
-		.attr("stroke-width", d => Math.sqrt(d.value));
+		const width = 1200;
+		const height = 1200;
 
-	const node = svg.append("g")
-		.attr("stroke", "#fff")
-		.attr("stroke-width", 1.5)
-		.selectAll("circle")
-		.data(nodes)
-		.join("circle")
-		.attr("r", 5)
-		.attr("fill", d => {
-			const scale = d3.scaleOrdinal(d3.schemeCategory10);
-			return scale(d.group);
+		const filteredLinks = selectedPackage ? links.filter(link => link.source.id === selectedPackage || link.target.id === selectedPackage) : links;
+		const filteredNodes = selectedPackage ? nodes.filter(node => node.id === selectedPackage || filteredLinks.findIndex(value => value.source.id === node.id || value.target.id === node.id) > -1) : nodes;
+
+		const scale = d3.scaleOrdinal(d3.schemeCategory10);
+	
+		const simulation = d3.forceSimulation(filteredNodes)
+			.force("link", d3.forceLink(filteredLinks).id(d => d.id))
+			.force("charge", d3.forceManyBody())
+			.force("center", d3.forceCenter(width / 2, height / 2));
+	
+		const svg = d3.select('#dependencyGraphContainer')
+			.append('svg')
+			.attr("id", "dependencyGraphContainerSvg")
+			.attr("width", width)
+			.attr('height', height);
+	
+		const link = svg.append("g")
+			.attr("stroke", "#999")
+			.attr("stroke-opacity", 0.6)
+			.selectAll("line")
+			.data(filteredLinks)
+			.join("line")
+			.attr("stroke-width", d => Math.sqrt(d.value));
+	
+		const node = svg.append("g")
+			.attr("stroke", "#fff")
+			.attr("stroke-width", 1.5)
+			.selectAll("circle")
+			.data(filteredNodes)
+			.join("circle")
+			.attr("r", 5)
+			.attr("fill", d => {
+				return scale(d.group);
+			});
+	
+		node.append("title")
+			.text(d => d.id);
+	
+		simulation.on("tick", () => {
+			link
+				.attr("x1", d => d.source.x)
+				.attr("y1", d => d.source.y)
+				.attr("x2", d => d.target.x)
+				.attr("y2", d => d.target.y);
+	
+			node
+				.attr("cx", d => d.x)
+				.attr("cy", d => d.y);
 		});
+	}
 
-	node.append("title")
-		.text(d => d.id);
+	const packagesSelect = document.getElementById('packages');
+	const defaultOption = document.createElement('option');
+	defaultOption.text = '';
+	packagesSelect.appendChild(defaultOption);
+	for (const node of nodes) {
+		const option = document.createElement('option');
+		option.text = node.id;
+		option.value = node.id;
+		packagesSelect.appendChild(option);
+	}
 
-	simulation.on("tick", () => {
-		link
-			.attr("x1", d => d.source.x)
-			.attr("y1", d => d.source.y)
-			.attr("x2", d => d.target.x)
-			.attr("y2", d => d.target.y);
-
-		node
-			.attr("cx", d => d.x)
-			.attr("cy", d => d.y);
+	packagesSelect.addEventListener('change', () => {
+		generateGraph(packagesSelect.value);
 	});
+
+	generateGraph(null);
+	
 	</script>
   </body>
   </html>`;
--- src/parser.ts
+++ src/parser.ts
@@ -1,8 +1,3 @@
-import { forceCenter, forceLink, forceManyBody, forceSimulation } from 'd3-force';
-import { drag, scaleOrdinal, schemeCategory10, select } from 'd3';
-import { Logger } from './logger';
-import { window } from 'vscode';
-
 interface DependencyNode {
     id: string;
     group: number;
@@ -17,97 +12,58 @@ interface DependencyLink {
 interface Dependency {
     version: string;
     dependencies: { [name: string]: Dependency };
+    requires: { [name: string]: string };
 }
 
 interface DependencyRoot extends Dependency {
     name: string;
 }
 
+interface PackageRoot {
+    dependencies: { [name: string]: string };
+    devDependencies: { [name: string]: string };
+    name: string;
+}
+
 export class DependenciesParser {
 
-    private static _nodes: DependencyNode[] = [];
-    private static _links: DependencyLink[] = [];
+    public static parse(packageRoot: PackageRoot, dependencyRoot: DependencyRoot): { nodes: DependencyNode[], links: DependencyLink[] } {
+        const result: { nodes: DependencyNode[], links: DependencyLink[] } = { nodes: [], links: [] };
+        result.nodes.push({ id: packageRoot.name, group: 1 });
 
-    private static readonly _width: number = 300;
-    private static readonly _height: number = 300;
-
-    public static parse(dependencyRoot: DependencyRoot): { nodes: DependencyNode[], links: DependencyLink[] } {
-        this._nodes.push({ id: dependencyRoot.name, group: 1 });
-
-        const readPkg = dependencyRoot.dependencies['read-pkg' as any];
-        Logger.info('Flattening dependencies');
-        this.flattenDependencies(dependencyRoot, dependencyRoot.name);
-
-        return { nodes: this._nodes, links: this._links };
-    }
-
-    private static flattenDependencies(dependency: Dependency, source: string): void {
-        for (const name in dependency.dependencies) {
-            Logger.info(`Analying dependency ${name} of ${source}`);
-            const nodeIndex = this._nodes.findIndex(value => value.id === name);
-            if (nodeIndex === -1) {
-                this._nodes.push({ group: 1, id: name });
-            } else {
-                this._nodes[nodeIndex].group++;
-            }
-
-            const linkIndex = this._links.findIndex(value => value.source === source && value.target === name);
-            if (linkIndex === -1) {
-                this._links.push({ source: source, target: name, value: 1 });
-            } else {
-                this._links[linkIndex].value++;
-            }
-
-            this.flattenDependencies(dependency.dependencies[name], name);
+        for (const name in packageRoot.dependencies) {
+            result.nodes.push({ id: name, group: 2 });
+            result.links.push({ source: packageRoot.name, target: name, value: 1 });
         }
+
+        for (const name in packageRoot.devDependencies) {
+            result.nodes.push({ id: name, group: 2 });
+            result.links.push({ source: packageRoot.name, target: name, value: 1 });
+        }
+
+        for (const name in dependencyRoot.dependencies) {
+            this.processDependency(name, dependencyRoot.dependencies[name], result, 3);
+        }
+
+        return result;
     }
 
-    private static generateGraph(): void {
-        try {
-            const simulation = forceSimulation(this._nodes as any[])
-                .force('link', forceLink(this._links as any[]).id(d => (d as any).id))
-                .force("charge", forceManyBody())
-                .force("center", forceCenter(this._width / 2, this._height / 2));
+    private static processDependency(name: string, dependency: Dependency, graph: { nodes: DependencyNode[], links: DependencyLink[] }, depth: number): void {
+        if (graph.nodes.findIndex(dep => dep.id === name) === -1) {
+            graph.nodes.push({ id: name, group: depth });
+        }
 
-            const svg = select('#dependencyGraphContainer')
-                .append('svg')
-                .attr("width", this._width)
-                .attr('height', this._height);
+        for (const dep in dependency.dependencies) {
+            if (graph.links.findIndex(link => link.source === name && link.target === dep) === -1) {
+                graph.links.push({ source: name, target: dep, value: 1 });
+                this.processDependency(dep, dependency.dependencies[dep], graph, depth + 1);
+            }
+        }
 
-            const link = svg.append("g")
-                .attr("stroke", "#999")
-                .attr("stroke-opacity", 0.6)
-                .selectAll("line")
-                .data(this._links)
-                .join("line")
-                .attr("stroke-width", d => Math.sqrt(d.value));
-
-            const node = svg.append("g")
-                .attr("stroke", "#fff")
-                .attr("stroke-width", 1.5)
-                .selectAll("circle")
-                .data(this._nodes)
-                .join("circle")
-                .attr("r", 5);
-            // .attr("fill", color)
-            // .call(drag());
-
-            node.append("title")
-                .text(d => d.id);
-
-            simulation.on("tick", () => {
-                link
-                    .attr("x1", d => (d.source as any).x)
-                    .attr("y1", d => (d.source as any).y)
-                    .attr("x2", d => (d.source as any).x)
-                    .attr("y2", d => (d.source as any).y);
-
-                node
-                    .attr("cx", d => (d as any).x)
-                    .attr("cy", d => (d as any).y);
-            });
-        } catch (error) {
-            Logger.info(error);
+        for (const dep in dependency.requires) {
+            if (graph.links.findIndex(link => link.source === name && link.target === dep) === -1) {
+                graph.links.push({ source: name, target: dep, value: 1 });
+            }
         }
     }
 
